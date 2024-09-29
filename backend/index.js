5afa92a6e8ea4303a190df0292a0096da368dee5
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-09-29 20:24:19 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,21 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: '*',
+    origin:  'https://movie-rating-k3yo.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
   }));
-  
+  app.options('*', cors());
+  app.use(cors());
+
+
 app.use(bodyParser.json());
 
 // Routes
 app.use('/api', movieRoutes);
 app.use('/api', reviewRoutes);
+
+
 
 // Start server
 connectDB();
