a0dedc1bb51efe53011b774b206ac979e8784deb
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-09-29 20:53:10 +0530

    update endpoint

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Routes
+
 app.use('/api', movieRoutes);
 app.use('/api', reviewRoutes);
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://movie-rating-two.vercel.app';
+const API_URL = 'https://movie-rating-two.vercel.app/api/movies';
 
 export const getMovies = async () => {
   const res = await axios.get(API_URL);
